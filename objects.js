e4e14f733a6ae8972a871264d65b2b4ff5d5ffdb
Sundar Maharjan <sundarmaharjan43@gmail.com>
2019-10-19 22:41:26 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,7 +6,7 @@ function updatePlaylist (playlist,artistName,songTitle){
   playlist[artistName] = songTitle
   return playlist
 }
- function removeFromPlaylist(playlist,artistName)
- delete playlist[artistname]
+ function removeFromPlaylist(playlist,artistName){
+ delete playlist [artistname]
  return playlist
  }
